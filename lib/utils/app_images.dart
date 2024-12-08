d34e670fc4aeac35cc444d130629e11b65a70643
Amira Tarek <amirat737@gmail.com>
2024-12-08 07:02:11 +0200

    generate app images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,61 @@
+abstract class Assets {
+  Assets._();
+
+  static const add = "assets/images/add.svg";
+
+  static const admin = "assets/images/admin.svg";
+
+  static const bell = "assets/images/bell.svg";
+
+  static const cards = "assets/images/cards.svg";
+
+  static const cloudcard = "assets/images/cloudcard.svg";
+
+  static const cloudcash = "assets/images/cloudcash.svg";
+
+  static const deacivateCard = "assets/images/deacivate_card.svg";
+
+  static const lectromenMarket = "assets/images/electromen_market..svg";
+
+  static const electronics = "assets/images/electronics.svg";
+
+  static const goals = "assets/images/goals.svg";
+
+  static const holidays = "assets/images/holidays.svg";
+
+  static const invoices = "assets/images/invoices.svg";
+
+  static const left = "assets/images/left.svg";
+
+  static const limit = "assets/images/limit.svg";
+
+  static const mail = "assets/images/mail.svg";
+
+  static const overview = "assets/images/overview.svg";
+
+  static const premium = "assets/images/premium.svg";
+
+  static const renovation = "assets/images/renovation.svg";
+
+  static const restaurant = "assets/images/restaurant.svg";
+
+  static const right = "assets/images/right.svg";
+
+  static const send = "assets/images/send.svg";
+
+  static const settings = "assets/images/settings.svg";
+
+  static const shoppping = "assets/images/shoppping.svg";
+
+  static const tescoMarket = "assets/images/tesco_market.svg";
+
+  static const transactions = "assets/images/transactions.svg";
+
+  static const travels = "assets/images/travels.svg";
+
+  static const upgrade = "assets/images/upgrade.svg";
+
+  static const user = "assets/images/user.svg";
+
+  static const xbox = "assets/images/xbox.svg";
+}
