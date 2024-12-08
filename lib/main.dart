d9b968884eccef6032fe29a4d97903dec222e95a
Amira Tarek <amirat737@gmail.com>
2024-12-08 02:15:31 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FinancialDashboard());
+}
+
+class FinancialDashboard extends StatelessWidget {
+  const FinancialDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
