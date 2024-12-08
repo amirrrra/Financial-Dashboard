aa75a77442b06c1ecf81bbf7334da885427077e9
Amira Tarek <amirat737@gmail.com>
2024-12-08 20:16:03 +0200

    apply responsive font size

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,77 +1,173 @@
-import 'package:financial_dashboard/utils/constants.dart';
+import 'package:financial_dashboard/utils/app_colors.dart';
+import 'package:financial_dashboard/utils/Constants.dart';
+import 'package:financial_dashboard/utils/size_config.dart';
 import 'package:flutter/material.dart';
 
 abstract class AppStyles {
-  static const regular8 = TextStyle(
-    fontSize: 8,
-  );
-  static const regular12 = TextStyle(
-    fontSize: 12,
-  );
-  static const regular14 = TextStyle(
-    fontSize: 14,
-  );
-  static const regular16 = TextStyle(
-    fontSize: 16,
-  );
-  static const regular20 = TextStyle(
-    fontSize: 20,
-  );
+  static TextStyle remaRegular8(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 8),
+    );
+  }
 
-  static const semi14 = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w600,
-  );
-  static const semi20 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-  );
-  static const semi44 = TextStyle(
-    fontSize: 44,
-    fontWeight: FontWeight.w600,
-  );
+  static TextStyle remaRegular12(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 12),
+    );
+  }
 
+  static TextStyle remaRegular14(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 14),
+    );
+  }
 
-  static const boldLato14 = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.bold,
-    fontFamily: Constants.lato,
-  );
-  static const boldLato8 = TextStyle(
-    fontSize: 8,
-    fontWeight: FontWeight.bold,
-    fontFamily: Constants.lato,
-  );
-  static const lightLato24 = TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeight.bold,
-    fontFamily: Constants.lato,
-  );
-  static const lightLato20 = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.bold,
-    fontFamily: Constants.lato,
-  );
-  static const semiLato24 = TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeight.w600,
-    fontFamily: Constants.lato,
-  );
-  static const semiLato14 = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w600,
-    fontFamily: Constants.lato,
-  );
-  static const mediumLato7 = TextStyle(
-    fontSize: 7,
-    fontWeight: FontWeight.w600,
-    fontFamily: Constants.lato,
-  );
-  static const mediumLato12 = TextStyle(
-    fontSize: 12,
-    fontWeight: FontWeight.w600,
-    fontFamily: Constants.lato,
-  );
+  static TextStyle remaRegular16(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 16),
+      color: AppColors.greyAe,
+    );
+  }
 
+  static TextStyle remaRegular20(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 20),
+    );
+  }
 
+  static TextStyle remaSemi14(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 14),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle remaSemi20(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 20),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle remaSemi44(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 44),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle latoBold14(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 14),
+      fontWeight: FontWeight.bold,
+      fontFamily: Constants.lato,
+    );
+  }
+
+  static TextStyle latoBold8(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 8),
+      fontWeight: FontWeight.bold,
+      fontFamily: Constants.lato,
+      color: AppColors.white,
+    );
+  }
+
+  static TextStyle latoLight24(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 24),
+      fontWeight: FontWeight.w300,
+      fontFamily: Constants.lato,
+      color: AppColors.greyC7,
+    );
+  }
+
+  static TextStyle latoLight20(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 20),
+      fontWeight: FontWeight.w300,
+      fontFamily: Constants.lato,
+      color: AppColors.greyC7,
+    );
+  }
+
+  static TextStyle latoSemi24(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 24),
+      fontWeight: FontWeight.w600,
+      fontFamily: Constants.lato,
+      color: AppColors.grey69,
+    );
+  }
+
+  static TextStyle latoSemi14(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 14),
+      fontWeight: FontWeight.w600,
+      fontFamily: Constants.lato,
+    );
+  }
+
+  static TextStyle latoMedium7(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 7),
+      fontWeight: FontWeight.w500,
+      fontFamily: Constants.lato,
+      color: AppColors.white,
+    );
+  }
+
+  static TextStyle latoMedium12(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 12),
+      fontWeight: FontWeight.w500,
+      fontFamily: Constants.lato,
+      color: AppColors.white,
+    );
+  }
+
+  static TextStyle quickMedium21(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 21),
+      fontWeight: FontWeight.w500,
+      fontFamily: Constants.quicksand,
+      color: AppColors.white,
+    );
+  }
+
+  static TextStyle quickBold11(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 11),
+      fontWeight: FontWeight.bold,
+      fontFamily: Constants.quicksand,
+    );
+  }
+
+  static TextStyle quickBold16(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, 16),
+      fontWeight: FontWeight.bold,
+      fontFamily: Constants.quicksand,
+      color: AppColors.white,
+    );
+  }
+}
+
+double getResponsiveFontSize(BuildContext context, double fontSize) {
+  var scaleFactor = getScaleFactor(context);
+  var responsiveFontSize = fontSize * scaleFactor;
+  var lowerLimit = fontSize * .8;
+  var upperLimit = fontSize * 1.2;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit); //Apply limitations
+}
+
+double getScaleFactor(BuildContext context) {
+  final double screenWidth = MediaQuery.sizeOf(context).width;
+  if (screenWidth < SizeConfig.tablet) {
+    return screenWidth / 550; // Mobile Scale Factor
+  } else if (screenWidth < SizeConfig.desktop) {
+    return screenWidth / 1000; // Tablet Scale Factor
+  } else {
+    return screenWidth / 1920; // Desktop Scale Factor
+  }
 }
