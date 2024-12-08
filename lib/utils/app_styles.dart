d7207a2bbfd5d00756785e54812b334739a70759
Amira Tarek <amirat737@gmail.com>
2024-12-08 06:29:57 +0200

    create app styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,77 @@
+import 'package:financial_dashboard/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const regular8 = TextStyle(
+    fontSize: 8,
+  );
+  static const regular12 = TextStyle(
+    fontSize: 12,
+  );
+  static const regular14 = TextStyle(
+    fontSize: 14,
+  );
+  static const regular16 = TextStyle(
+    fontSize: 16,
+  );
+  static const regular20 = TextStyle(
+    fontSize: 20,
+  );
+
+  static const semi14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static const semi20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static const semi44 = TextStyle(
+    fontSize: 44,
+    fontWeight: FontWeight.w600,
+  );
+
+
+  static const boldLato14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    fontFamily: Constants.lato,
+  );
+  static const boldLato8 = TextStyle(
+    fontSize: 8,
+    fontWeight: FontWeight.bold,
+    fontFamily: Constants.lato,
+  );
+  static const lightLato24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    fontFamily: Constants.lato,
+  );
+  static const lightLato20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontFamily: Constants.lato,
+  );
+  static const semiLato24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: Constants.lato,
+  );
+  static const semiLato14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: Constants.lato,
+  );
+  static const mediumLato7 = TextStyle(
+    fontSize: 7,
+    fontWeight: FontWeight.w600,
+    fontFamily: Constants.lato,
+  );
+  static const mediumLato12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    fontFamily: Constants.lato,
+  );
+
+
+}
